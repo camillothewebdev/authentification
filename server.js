e638f7f10cf23fbe0e52210cabcd76f075ce2cec
camillothewebdev <camillothewebdev@gmail.com>
2025-07-17 11:29:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ async function demarrerBaileys() {
     if (connection === "close") {
       console.log("Connexion fermée. Reconnexion :", connectedToWhatsapp);
       if (!connectedToWhatsapp) {
+           fs.rmSync("auth_info", { recursive: true, force: true });
         await demarrerBaileys();
       } else {
         console.log("❌ Auth échouée. Rescanner QR manuellement.");
